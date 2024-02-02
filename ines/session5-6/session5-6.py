ff9682e6813bb8ab83df2e45da8adff980e16427
inesrienzo <irienzo.ieu2022@student.ie.edu>
2024-02-02 11:05:06 +0100

    t

--- ines/session5-6/session5-6.py
+++ ines/session5-6/session5-6.py
@@ -1,6 +1,7 @@
 name = input("What is your name?")
 age = input("How old are you?")
 # print("Hello,", name, "!", sep="")
+
 age = int(age)  # convert string to integer
 birth_year = 2023 - age
 print(name, ", You were born in", birth_year, ".", sep="")
